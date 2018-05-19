dbd7f63b8cabe4bbd9762f11b6f6cb9c4edae252
jtorriccelli1986@gmail.com <jtorriccelli1986@gmail.com>
2018-05-19 11:22:30 -0500

    update

--- modulo 03/clase 04/consumiendo servicios/js/main.js
+++ modulo 03/clase 04/consumiendo servicios/js/main.js
@@ -1,4 +1,5 @@
 $("#usuarios").click(function(){
+	$(".contenido").html("");
 	var url="https://jsonplaceholder.typicode.com/users";
 	//$.getJSON("url","funcion al finalizar")
 	$.getJSON(url,function(data){
@@ -9,6 +10,8 @@ $("#usuarios").click(function(){
 			console.log(item.username)
 			console.log(item.address.geo.lat)
 			console.log(item.address.geo.lng)
+			$(".contenido").append(`<article> <h2>`+item.name+`</h2> <small>`+item.username+`</small></article>`)
+			
 		}
 		
 		
