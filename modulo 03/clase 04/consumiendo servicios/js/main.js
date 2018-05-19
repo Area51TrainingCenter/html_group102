cc0fe65f68295c9714d1c648c65f5809e4873404
jtorriccelli1986@gmail.com <jtorriccelli1986@gmail.com>
2018-05-19 12:23:28 -0500

    update

--- modulo 03/clase 04/consumiendo servicios/js/main.js
+++ modulo 03/clase 04/consumiendo servicios/js/main.js
@@ -1,19 +1,35 @@
 $("#usuarios").click(function(){
-	$(".contenido").html("");
+	$(".contenido ul").html("");
 	var url="https://jsonplaceholder.typicode.com/users";
 	//$.getJSON("url","funcion al finalizar")
 	$.getJSON(url,function(data){
 		for (var i = 0; i < 10; i++) {
 			var item=data[i];
-			console.log(item.id)
+			/*console.log(item.id)
 			console.log(item.name)
 			console.log(item.username)
 			console.log(item.address.geo.lat)
-			console.log(item.address.geo.lng)
-			$(".contenido").append(`<article> <h2>`+item.name+`</h2> <small>`+item.username+`</small></article>`)
-			
+			console.log(item.address.geo.lng)*/
+			$(".contenido ul").append(`<li onclick="cargardata(this)" class="item-usuario" data-lat="`+item.address.geo.lat+`" data-lng="`+item.address.geo.lng+`">
+				<h2>`+item.name+`</h2>
+				<small>`+item.username+`</small>
+			</li>`)
+
 		}
 		
+
+		/*$(".item-usuario").click(function(){
+			alert("click");
+		})*/
+
+
 		
-	})
+	}) // termina el metodo getjson
+
 })
+
+function cargardata(valor){
+
+	console.log($(valor).attr("data-lng"))
+	console.log($(valor).attr("data-lat"))
+}
